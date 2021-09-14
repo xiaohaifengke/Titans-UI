861afdbd2e5dd53766437ae6342c7b353942606c
Jhail <Jhail@foxmail.com>
2021-09-14 14:48:50 +0800

    feat: 添加图标列表页

--- story/pages/normal/icon.vue
+++ story/pages/normal/icon.vue
@@ -1,13 +1,36 @@
 <template>
-  <ti-icon icon="edit"></ti-icon>
+  <ul class="icon-list">
+    <li
+        v-for="icon in icons"
+        :key="icon"
+        class="icon-wrapper">
+      <ti-icon :icon="icon" />
+      <div class="name">{{icon}}</div>
+    </li>
+  </ul>
 </template>
 
-<script>
-export default {
-
-};
+<script setup>
+import { icons } from './icons';
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.icon-list {
+  margin: 0;
+  padding: 0;
+}
+.icon-wrapper {
+  display: inline-block;
+  width: 100px;
+  font-size: 12px;
+  text-align: center;
+}
+:deep(.ti-icon) {
+  font-size: 42px;
+  height: 76px;
+  line-height: 76px;
+}
+.name {
+  line-height: 24px;
+}
 </style>
