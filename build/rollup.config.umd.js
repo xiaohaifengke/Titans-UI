a968457222db1e97a50d316e4bc9ec72a68997ce
Jhail <Jhail@foxmail.com>
2021-10-08 10:59:35 +0800

    build: rollup's umd config

--- build/rollup.config.umd.js
+++ build/rollup.config.umd.js
@@ -4,14 +4,20 @@ import typescript from 'rollup-plugin-typescript2'
 import scss from 'rollup-plugin-scss'
 import * as path from 'path'
 import vue from 'rollup-plugin-vue'
+import { terser } from 'rollup-plugin-terser'
 
+const env = process.env.NODE_ENV
 const outDir = 'dist'
 export default {
   input: path.resolve(__dirname, '../src/index.ts'),
   output: {
-    dir: outDir,
+    file: `${outDir}/index${env === 'production' ? '.min' : ''}.js`,
     format: 'umd',
-    name: 'TitansUI'
+    name: 'TitansUI',
+    exports: 'named',
+    globals: {
+      vue: 'Vue'
+    }
   },
   plugins: [
     vue({
@@ -29,7 +35,8 @@ export default {
       outputStyle: 'compressed',
       // processor: () => postcss([autoprefixer({ overrideBrowserslist: 'Edge 18' })]),
       prefix: `@import "src/style/global-import.scss";`
-    })
+    }),
+    env === 'production' && terser()
   ],
   external(id) {
     return /^vue/.test(id)
