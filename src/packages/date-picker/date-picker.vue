fb8acdfc3b497b4605656e68bca17240346f52b1
Jhail <Jhail@foxmail.com>
2021-10-27 18:01:28 +0800

    feat: temp

--- src/packages/date-picker/date-picker.vue
+++ src/packages/date-picker/date-picker.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="ti-date-picker">
-    <input class="ti-date-picker_input--date" :class="classes" type="text" />
-    <span class="ti-date-picker_input--prefix"></span>
-    <span class="ti-date-picker_input--suffix"></span>
+  <div class="ti-date-picker" v-click-outside="hidePanel">
+    <TiInput prefix-icon="date" @focus="handleFocus" />
+    <!--    <input class="ti-date-picker_input&#45;&#45;date" :class="classes" type="text" />-->
+    <!--    <span class="ti-date-picker_input&#45;&#45;prefix"></span>-->
+    <!--    <span class="ti-date-picker_input&#45;&#45;suffix"></span>-->
     <div class="ti-date-picker_popper" v-if="datePickerPanelVisible">
       <div class="ti-date-picker_wrapper">
         <div class="ti-date-picker_header">
@@ -13,16 +14,90 @@
           <ti-icon icon="arrow-right" />
           <ti-icon icon="d-arrow-right" />
         </div>
+        <div class="ti-date-picker_body">
+          <table>
+            <thead class="ti-date-picker_body--head">
+              <tr>
+                <td>日</td>
+                <td>一</td>
+                <td>二</td>
+                <td>三</td>
+                <td>四</td>
+                <td>五</td>
+                <td>六</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="row in 6" :key="row">
+                <td v-for="col in 7" :key="col">
+                  {{ dates[(row - 1) * 6 + col - 1].day }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref, computed } from 'vue'
+import dayjs from 'dayjs'
+import clickOutside from '../../utils/clickOutside'
 import TiIcon from '../icon'
+import TiInput from '../input'
 
+// 显示或隐藏日历面板
+const focusHandler = () => {
+  const datePickerPanelVisible = ref(false)
+  const handleFocus = () => {
+    datePickerPanelVisible.value = true
+  }
+  const hidePanel = () => {
+    console.log('hidePanel')
+    datePickerPanelVisible.value = false
+  }
+
+  return {
+    datePickerPanelVisible,
+    handleFocus,
+    hidePanel
+  }
+}
+
+// 生成日期列表
+interface DateInfo {
+  t: any
+  date: string
+  day: number
+  week: number
+  month: number
+}
+const useGenerateDays = (date?: string | number | undefined): DateInfo[] => {
+  date = date || Date.now()
+  console.log(dayjs(date))
+  const firstDateOfCurrentMonth = dayjs().date(1)
+  const firstDayOfCurrentMonth = firstDateOfCurrentMonth.day()
+  const startDate = firstDateOfCurrentMonth.subtract(
+    firstDayOfCurrentMonth || 7,
+    'day'
+  )
+  const dates = Array.from({ length: 42 }).map((d, i) => {
+    const t = startDate.add(i, 'day')
+    return {
+      t,
+      date: t.format('YYYY-MM-DD'),
+      day: t.date(),
+      week: t.day(),
+      month: t.month() + 1
+    }
+  })
+  console.log(dates)
+  return dates
+}
 export default defineComponent({
   name: 'TiDatePicker',
+  directives: { clickOutside },
   props: {
     placeholder: {
       type: String
@@ -32,7 +107,7 @@ export default defineComponent({
       default: 'normal'
     }
   },
-  components: { TiIcon },
+  components: { TiIcon, TiInput },
   setup(props) {
     const classes = computed(() => {
       return {
@@ -42,12 +117,16 @@ export default defineComponent({
 
     const selectYear = ref(2021)
     const selectMonth = ref(10)
-    const datePickerPanelVisible = ref(false)
+
+    const showPanel = focusHandler()
+    const dates = useGenerateDays()
+
     return {
       selectYear,
       selectMonth,
       classes,
-      datePickerPanelVisible
+      ...showPanel,
+      dates
     }
   }
 })
---
+++ src/utils/clickOutside.ts
@@ -0,0 +1,152 @@
+// Note: temp use in vue3
+// vue2 version is https://github.com/ndelvalle/v-click-outside
+import { SimpleFunction } from '../shims'
+
+const HANDLERS_PROPERTY = '__v-click-outside'
+const HAS_WINDOWS = typeof window !== 'undefined'
+const HAS_NAVIGATOR = typeof navigator !== 'undefined'
+const IS_TOUCH =
+  HAS_WINDOWS &&
+  ('ontouchstart' in window ||
+    (HAS_NAVIGATOR && navigator.msMaxTouchPoints > 0))
+const EVENTS = IS_TOUCH ? ['touchstart'] : ['click']
+
+type Binding = {
+  value: any
+  oldValue: any
+}
+type BindingValue = {
+  handler: SimpleFunction
+  middleware: SimpleFunction
+  events: string[]
+  isActive: boolean
+  detectIframe: any
+  capture: boolean
+} & SimpleFunction
+
+type EventOptions = {
+  el?: HTMLBaseElement
+  event: any
+  handler: SimpleFunction
+  middleware: SimpleFunction
+}
+
+type EventParams = EventOptions & { srcTarget: HTMLElement; capture: boolean }
+function processDirectiveArguments(bindingValue: BindingValue) {
+  const isFunction = typeof bindingValue === 'function'
+  if (!isFunction && typeof bindingValue !== 'object') {
+    throw new Error(
+      'v-click-outside: Binding value must be a function or an object'
+    )
+  }
+
+  return {
+    handler: isFunction ? bindingValue : bindingValue.handler,
+    middleware: bindingValue.middleware || ((item: any) => item),
+    events: bindingValue.events || EVENTS,
+    isActive: !(bindingValue.isActive === false),
+    detectIframe: !(bindingValue.detectIframe === false),
+    capture: !!bindingValue.capture
+  }
+}
+
+function execHandler({ event, handler, middleware }: EventOptions) {
+  if (middleware(event)) {
+    handler(event)
+  }
+}
+
+function onFauxIframeClick({ el, event, handler, middleware }: EventOptions) {
+  // Note: on firefox clicking on iframe triggers blur, but only on
+  //       next event loop it becomes document.activeElement
+  // https://stackoverflow.com/q/2381336#comment61192398_23231136
+  setTimeout(() => {
+    const { activeElement } = document
+    if (
+      activeElement &&
+      activeElement.tagName === 'IFRAME' &&
+      !el!.contains(activeElement)
+    ) {
+      execHandler({ event, handler, middleware })
+    }
+  }, 0)
+}
+
+function onEvent({ el, event, handler, middleware }: EventOptions) {
+  // Note: composedPath is not supported on IE and Edge, more information here:
+  //       https://developer.mozilla.org/en-US/docs/Web/API/Event/composedPath
+  //       In the meanwhile, we are using el.contains for those browsers, not
+  //       the ideal solution, but using IE or EDGE is not ideal either.
+  const path = event.path || (event.composedPath && event.composedPath())
+  const isClickOutside = path
+    ? path.indexOf(el) < 0
+    : !el!.contains(event.target)
+
+  if (!isClickOutside) {
+    return
+  }
+
+  execHandler({ event, handler, middleware })
+}
+
+function created(el: any, { value }: Binding) {
+  const { events, handler, middleware, isActive, detectIframe, capture } =
+    processDirectiveArguments(value)
+  if (!isActive) {
+    return
+  }
+
+  el[HANDLERS_PROPERTY] = events.map((eventName) => ({
+    event: eventName,
+    srcTarget: document.documentElement,
+    handler: (event: Event) => onEvent({ el, event, handler, middleware }),
+    capture
+  }))
+
+  if (detectIframe) {
+    const detectIframeEvent = {
+      event: 'blur',
+      srcTarget: window,
+      handler: (event: Event) =>
+        onFauxIframeClick({ el, event, handler, middleware }),
+      capture
+    }
+
+    el[HANDLERS_PROPERTY] = [...el[HANDLERS_PROPERTY], detectIframeEvent]
+  }
+
+  el[HANDLERS_PROPERTY].forEach(({ event, srcTarget, handler }: EventParams) =>
+    setTimeout(() => {
+      // Note: More info about this implementation can be found here:
+      //       https://github.com/ndelvalle/v-click-outside/issues/137
+      if (!el[HANDLERS_PROPERTY]) {
+        return
+      }
+      srcTarget.addEventListener(event, handler, capture)
+    }, 0)
+  )
+}
+
+function unmounted(el: any) {
+  const handlers = el[HANDLERS_PROPERTY] || []
+  handlers.forEach(({ event, srcTarget, handler, capture }: EventParams) =>
+    srcTarget.removeEventListener(event, handler, capture)
+  )
+  delete el[HANDLERS_PROPERTY]
+}
+
+function beforeUpdate(el: any, { value, oldValue }: Binding) {
+  if (JSON.stringify(value) === JSON.stringify(oldValue)) {
+    return
+  }
+  unmounted(el)
+  created(el, { value, oldValue })
+}
+
+const directive = {
+  created,
+  beforeUpdate,
+  unmounted
+}
+
+export default HAS_WINDOWS ? directive : {}
