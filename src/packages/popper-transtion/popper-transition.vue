e8bc47f1c234f6f2495c50fa19690fa10e60be8f
Jhail <Jhail@foxmail.com>
2021-11-22 17:22:18 +0800

    feat: 重构popper-transition组件

--- src/packages/popper-transtion/popper-transition.vue
+++ src/packages/popper-transtion/popper-transition.vue
@@ -2,17 +2,20 @@
   <Teleport :disabled="!teleportToBody" to="body">
     <Transition
       name="popper-slide"
-      :duration="250"
+      :duration="duration"
       @after-enter="afterEnter"
       @after-leave="afterLeave"
     >
       <div
+        :class="{ 'ti-popper-transition-active': popperTransitionInitialized }"
         class="ti-popper-transition"
         ref="tooltipRef"
         v-show="visible"
         v-click-outside="vClickOutsideParams"
       >
-        <slot :tooltipRef="tooltipRef"></slot>
+        <div class="ti-popper-transition_content">
+          <slot :tooltipRef="tooltipRef"></slot>
+        </div>
         <div v-if="showArrow" class="ti-popper_arrow" data-popper-arrow></div>
       </div>
     </Transition>
@@ -20,8 +23,19 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue'
+import {
+  computed,
+  defineComponent,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+  nextTick,
+  watch
+} from 'vue'
 import clickOutside from '../../utils/clickOutside'
+import { createPopper } from '@popperjs/core'
+import { Instance } from '@popperjs/core/lib/types'
+import { Placement } from '@popperjs/core'
 
 export default defineComponent({
   name: 'TiPopperTransition',
@@ -31,6 +45,9 @@ export default defineComponent({
       type: Boolean,
       default: true
     },
+    reference: {
+      required: true
+    },
     vClickOutsideExtraEls: {
       type: Array,
       default: () => []
@@ -38,11 +55,15 @@ export default defineComponent({
     showArrow: {
       type: Boolean,
       default: true
+    },
+    placement: {
+      type: String,
+      default: 'bottom-start'
     }
   },
   emits: ['after-enter', 'after-leave'],
   setup(props, { emit }) {
-    const visible = ref(false)
+    const visible = ref(true)
     const show = () => {
       visible.value = true
     }
@@ -52,7 +73,7 @@ export default defineComponent({
     const vClickOutsideActive = ref(false)
     const vClickOutsideParams = computed(() => ({
       handler: hide,
-      extraEls: props.vClickOutsideExtraEls,
+      extraEls: [...props.vClickOutsideExtraEls, props.reference],
       isActive: vClickOutsideActive.value
     }))
 
@@ -69,7 +90,75 @@ export default defineComponent({
     }
 
     const tooltipRef = ref(null as any)
+
+    const { duration, popperTransitionInitialized } =
+      popperTransitionInitialize()
+
+    function popperTransitionInitialize() {
+      const duration = ref(250)
+      const popperTransitionInitialized = ref(false)
+      let scrollHeight: number
+      let popperInstance: Instance
+      onMounted(() => {
+        scrollHeight = tooltipRef.value.scrollHeight
+        hide()
+        setTimeout(() => {
+          popperTransitionInitialized.value = true
+        }, duration.value)
+
+        const modifiers = [
+          {
+            name: 'flip',
+            options: {
+              padding: scrollHeight
+            }
+          },
+          {
+            name: 'computeStyles',
+            options: {
+              gpuAcceleration: false
+            }
+          },
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 10]
+            }
+          }
+        ]
+
+        nextTick(() => {
+          popperInstance = createPopper(
+            props.reference as Element,
+            tooltipRef.value,
+            {
+              placement: props.placement as Placement,
+              modifiers
+            }
+          )
+        })
+      })
+
+      // 手动更新，不然会出现arrow定位不准的问题
+      watch(
+        () => visible.value,
+        () => {
+          popperInstance && popperInstance.update()
+        }
+      )
+
+      onBeforeUnmount(() => {
+        popperInstance.destroy()
+      })
+
+      return {
+        duration,
+        popperTransitionInitialized
+      }
+    }
     return {
+      duration,
+      popperTransitionInitialized,
       tooltipRef,
       visible,
       show,
@@ -88,17 +177,12 @@ export default defineComponent({
   .popper-slide-enter-active,
   .popper-slide-leave-active {
     opacity: 1;
-    transition: opacity 0.25s ease;
-    .ti-date-picker_popper {
-      transform: scaleY(1);
-    }
+    transform: scaleY(1);
   }
   .popper-slide-enter-from,
   .popper-slide-leave-to {
     opacity: 0;
-    .ti-date-picker_popper {
-      transform: scaleY(0);
-    }
+    transform: scaleY(0);
   }
 
   .ti-popper_arrow,
@@ -121,31 +205,53 @@ export default defineComponent({
 
   .ti-popper-transition {
     z-index: 500;
+    visibility: hidden;
     background-color: rgb(255, 255, 255);
     border-radius: map-get($defaultThemeMap, default-border-radius);
-    box-shadow: rgb(0 0 0 / 20%) 0 2px 10px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
 
-    &[data-popper-placement^='top'] > .ti-popper_arrow {
-      bottom: -4px;
-      &::before {
-        box-shadow: 2px 2px 5px -1px rgba(0, 0, 0, 0.2);
+    &[data-popper-placement^='top'] {
+      transform-origin: center bottom;
+
+      .ti-popper_arrow {
+        bottom: -4px;
+        &::before {
+          box-shadow: 2px 2px 5px -1px rgba(0, 0, 0, 0.2);
+        }
       }
     }
 
-    &[data-popper-placement^='bottom'] > .ti-popper_arrow {
-      top: -4px;
-      &::before {
-        box-shadow: -2px -2px 5px -1px rgba(0, 0, 0, 0.2);
+    &[data-popper-placement^='bottom'] {
+      transform-origin: center top;
+
+      .ti-popper_arrow {
+        top: -4px;
+        &::before {
+          box-shadow: -2px -2px 5px -1px rgba(0, 0, 0, 0.2);
+        }
       }
     }
 
-    &[data-popper-placement^='left'] > .ti-popper_arrow {
+    &[data-popper-placement^='left'] .ti-popper_arrow {
       right: -4px;
     }
 
-    &[data-popper-placement^='right'] > .ti-popper_arrow {
+    &[data-popper-placement^='right'] .ti-popper_arrow {
       left: -4px;
     }
+  }
+
+  .ti-popper-transition_content {
+    box-sizing: border-box;
+    display: inline-flex;
+    padding-top: 10px;
+    font-size: map-get($defaultThemeMap, fontsize);
+    color: map-get($defaultThemeMap, default);
+    user-select: none;
+  }
+  .ti-popper-transition-active {
+    visibility: visible;
+    transition: all 0.25s ease;
   }
 }
 </style>
