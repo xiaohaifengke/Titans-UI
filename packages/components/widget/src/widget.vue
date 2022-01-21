2f08f02072035ee729d8945f46525334f7a70542
Jhail <Jhail@foxmail.com>
2022-01-21 14:45:58 +0800

    feat(widget/screenfull): 新增widget/screenfull组件

--- packages/components/widget/src/widget.vue
+++ packages/components/widget/src/widget.vue
@@ -1,6 +1,12 @@
 <template>
   <component class="ti-widget" :is="name">
     <slot></slot>
+    <template #exit-fullscreen-icon>
+      <slot name="exit-fullscreen-icon"></slot>
+    </template>
+    <template #fullscreen-icon>
+      <slot name="fullscreen-icon"></slot>
+    </template>
   </component>
 </template>
 
@@ -9,10 +15,11 @@ import { defineComponent } from 'vue'
 import voice from '@titans-ui/components/widget/src/widgets/voice/voice.vue'
 import loading from '@titans-ui/components/widget/src/widgets/loading/loading.vue'
 import marquee from '@titans-ui/components/widget/src/widgets/marquee/marquee.vue'
+import screenfull from '@titans-ui/components/widget/src/widgets/screenfull/screenfull.vue'
 
 export default defineComponent({
   name: 'TiWidget',
-  components: { voice, loading, marquee },
+  components: { voice, loading, marquee, screenfull },
   props: {
     name: String
   }
