1bf7bcd7dbc4f2450b79914cbd8ae18398a2e820
Jhail <Jhail@foxmail.com>
2022-01-21 09:55:29 +0800

    feat(widget/marquee): 新增widget组件的子组件marquee

--- packages/components/widget/src/widget.vue
+++ packages/components/widget/src/widget.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="ti-widget">
-    <component :is="name" v-bind="$attrs"></component>
-  </div>
+  <component class="ti-widget" :is="name">
+    <slot></slot>
+  </component>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import voice from './widgets/voice/voice.vue'
-import loading from './widgets/loading/loading.vue'
+import voice from '@titans-ui/components/widget/src/widgets/voice/voice.vue'
+import loading from '@titans-ui/components/widget/src/widgets/loading/loading.vue'
+import marquee from '@titans-ui/components/widget/src/widgets/marquee/marquee.vue'
 
 export default defineComponent({
   name: 'TiWidget',
-  inheritAttrs: false,
-  components: { voice, loading },
+  components: { voice, loading, marquee },
   props: {
     name: String
   }
