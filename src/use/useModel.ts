3e1933e191cffa65bb0f49d3042f73c3d875b3ce
Jhail <Jhail@foxmail.com>
2022-02-08 16:53:44 +0800

    chore: remove some useless modules

--- src/use/useModel.ts
+++
@@ -1,6 +0,0 @@
-import { computed, ComputedRef, Ref } from 'vue'
-
-export function useModel<T>(get: () => T, set: (value: T) => void): Ref<T> {
-  const model = computed({ get: get, set: set })
-  return model
-}
