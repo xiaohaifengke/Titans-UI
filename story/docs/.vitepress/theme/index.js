0b4a85f85b2a3455c2d9e9d148b89d7f4f20cb9e
Jhail <Jhail@foxmail.com>
2022-02-10 23:48:33 +0800

    docs(button): 新增button组件的文档

--- story/docs/.vitepress/theme/index.js
+++ story/docs/.vitepress/theme/index.js
@@ -1,15 +1,11 @@
-// import DefaultTheme from 'vitepress/theme'
-// import './custom.css'
-//
-// export default DefaultTheme
-
-import TiTansUI from 'titans-ui'
-import 'titans-ui/dist/index.css'
+// import theme from 'vitepress/theme'
 import theme from 'vitepress/dist/client/theme-default'
 import 'vitepress-theme-demoblock/theme/styles/index.css'
-// import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import './styles/index.scss'
 import { registerComponents } from './register-components'
+import TiTansUI from 'titans-ui'
+import 'titans-ui/dist/index.css'
 
 export default {
   ...theme,
